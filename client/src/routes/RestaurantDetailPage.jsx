08dd2b70842969e8a6cf52a32297f39de0cd3739
Charles Nguyen <nguyencharles@live.ca>
2020-11-30 17:47:36 -0500

    Added reviews table. Implemented stars ratings component and cards for restaurant reviews in detailed page.

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { RestaurantsContext } from '../context/RestaurantsContext';
 import RestaurantApi from '../apis/RestaurantApi';
+import StarRating from '../components/StarRating';
+import Reviews from '../components/Reviews';
 
 const RestaurantDetailPage = () => {
   const { id } = useParams();
@@ -24,6 +26,13 @@ const RestaurantDetailPage = () => {
   return (
     <div>
       <h1 className="font-weight-light display-1 text-center">{selectedRestaurant && selectedRestaurant.name}</h1>
+      <div>{selectedRestaurant && (
+        <>
+        <div className="mt-3">
+          <Reviews />
+        </div>
+        </>
+      )}</div>
     </div>
   );
 };
--- server/db/db.sql
+++ server/db/db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE restaurants (
-    id BIGSERIAL NOT NULL,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     location VARCHAR(50) NOT NULL,
     price_range INT NOT NULL check (price_range >= 1 and price_range <= 5)
@@ -8,3 +8,15 @@ CREATE TABLE restaurants (
 INSERT INTO restaurants (name, location, price_range) VALUES ('McDonalds', 'New York', 2);
 INSERT INTO restaurants (name, location, price_range) VALUES ('Pizza Hut', 'Minnesota', 2);
 INSERT INTO restaurants (name, location, price_range) VALUES ('Wendys', 'Toronto', 1);
+
+CREATE TABLE reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    name VARCHAR(50) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT check(rating >= 1 and rating <= 5)
+);
+
+INSERT INTO reviews (name, restaurant_id, review, rating) VALUES ('Zayn', 2, 'bad', 1);
+INSERT INTO reviews (name, restaurant_id, review, rating) VALUES ('Simba', 2, 'great', 4);
+INSERT INTO reviews (name, restaurant_id, review, rating) VALUES ('Moka', 2, 'WOW', 5);
