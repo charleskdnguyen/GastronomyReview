4cd271544fd11d70afbe0613dd572194ffe4f8a0
Charles Nguyen <nguyencharles@live.ca>
2020-11-30 15:01:49 -0500

    Added context for application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 import Home from './routes/Home';
 import RestaurantDetail from './routes/RestaurantDetail';
 import RestaurantUpdate from './routes/RestaurantUpdate';
 
 const App = () => {
-  return <div className="container">
-      <Router>
-        <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/restaurants/:id/update" component={RestaurantUpdate}/>
-        <Route exact path="/restaurants/:id" component={RestaurantDetail}/>
-        </Switch>
-      </Router>
-    </div>
-}
+  return (
+    <RestaurantsContextProvider>
+      <div className='container'>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route
+              exact
+              path='/restaurants/:id/update'
+              component={RestaurantUpdate}
+            />
+            <Route exact path='/restaurants/:id' component={RestaurantDetail} />
+          </Switch>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
+  );
+};
 
 export default App;
